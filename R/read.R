7968a3fba58e47cb7780d62cc0cd966f9d447e57
Cancer Atlas <canceratlas@dyn-130-194-61-28.its.monash.edu.au>
2018-06-05 17:05:30 +1000

    variable subsetting

--- R/read.R
+++ R/read.R
@@ -1,4 +1,17 @@
 library(tidyverse)
 
-fishes <- read_csv("data/Fishes.zip")
-colnames(fishes)
+fishes <- read_csv("data/Fishes.csv.zip")
+View(colnames(fishes))
+
+fishes_sub <- fishes %>% dplyr::select(id = `Record ID`,
+                                data_resource = `Data Resource ID`,
+                                scientificName,
+                                taxon_concept_lsid = `taxonConceptID`,
+                                taxon_rank = taxonRank,
+                                kingdom, phylum, class, order, family, genus,
+                                decimalLatitude,decimalLongitude, geodeticDatum,
+                                sex, aus_conservation = austConservation.p,
+                                state_conservation = stateConservation.p,
+                                state = `Australian States and Territories`,
+                                eventDate, maximumDepthInMeters, minimumDepthInMeters,
+                                lifeStage, basisOfRecord)
