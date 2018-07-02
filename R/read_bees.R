8e2c86e2c631c656c9f99bf9c64e865b7562c10f
Mitchell <mitch.ohara-wild@monash.edu>
2018-07-02 17:17:30 +1000

    Updated data reading script

--- R/read_bees.R
+++ R/read_bees.R
@@ -1,33 +1,47 @@
-bee <- as_tibble(search_fulltext("bee")$data)
+library(ALA4R)
+library(tidyverse)
+library(lubridate)
 
-query_response <- occurrences(
-  fq = c("family:Apidae"),
-  qa = "none"
+# cache to a permanent dir
+ala_config(
+  cache_directory = "data_cache",
+  download_reason_id = 11, # "citizen science"
+  warn_on_empty = TRUE
 )
 
-machine_bees <- as_tibble(occurrences(
+bee <- as_tibble(search_fulltext("Sunflo")$data)
+
+# sunflower_obs <- as_tibble(occurrences(
+#   fq = c("family:Asteraceae"),
+#   wkt = "POLYGON((112.27030754089 -10.661994277738, 153.88651847839 -10.445985021005,154.06229972839 -44.27768127617,112.35819816589 -44.309136146459,112.35819816589 -10.748355247628,112.13847160339 -10.877850325879,112.27030754089 -10.661994277738))",
+#   qa = "none"
+# )$data)
+
+
+bee_obs <- as_tibble(occurrences(
   fq = c("family:Apidae"),
   wkt = "POLYGON((112.27030754089 -10.661994277738, 153.88651847839 -10.445985021005,154.06229972839 -44.27768127617,112.35819816589 -44.309136146459,112.35819816589 -10.748355247628,112.13847160339 -10.877850325879,112.27030754089 -10.661994277738))",
   qa = "none"
 )$data)
 
-machine_bees %>%
+
+bee_obs %>%
   count(scientificName) %>%
   arrange(desc(n))
 
-
-machine_obs %>%
-  filter(scientificName == "Amegilla (Zonamegilla) pulchra")
-
-
-machine_bees %>%
+bee_obs %>%
   count(genus)
+
+bee_obs$year %>% is.na %>% mean
 
 library(leaflet)
 pal <- colorFactor("Dark2", domain = NULL)
-leaflet(machine_bees %>% filter(!(genus%in%c("Ctenoplectra","Nomada", "")))) %>%
+leaflet(bee_obs %>% filter(!(genus%in%c("Ctenoplectra","Nomada", "")))) %>%
   addTiles() %>%
+  clearControls() %>%
   addCircleMarkers(lng = ~ longitude, lat = ~ latitude, color = ~ pal(genus)) %>%
   addLegend(pal = pal, values = ~ genus)
 
 
+# Hmm, interesting. Seasonal effect for Aussie (?) bees!
+bee_obs %>% filter(genus != "Apis") %>% count(month, state)  %>% ggplot(aes(x=month, y=n)) + geom_col() + facet_wrap(~ state)
