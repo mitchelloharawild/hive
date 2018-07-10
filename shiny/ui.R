a4d3445627de4f5c90f7c18578f5813878ef7627
Mitchell <mitch.ohara-wild@monash.edu>
2018-07-10 21:48:04 +1000

    Removed ui junk

--- shiny/ui.R
+++ shiny/ui.R
@@ -24,10 +24,6 @@ shinyUI(
       uiOutput("ui_info")
     ),
     dashboardBody(
-      # includeScript("https://d3js.org/d3-hexbin.v0.2.min.js"),
-      # includeScript("https://d3js.org/d3.v4.min.js"),
-      # includeScript("https://rawgit.com/Asymmetrik/leaflet-d3/master/dist/leaflet-d3.js"),
-      tags$link(rel="import", href="bower_components/emoji-rain/emoji-rain.html"),
       includeCSS("www/emu.css"),
       div(class="col-sm-8", style = "height:100%; padding-left:0; padding-right:0;",
           leafletOutput("map", width = "100%", height = "100%")
